bae9e1a080ab938ab7890a5f0a87afd2e4ffa97c
unifwizidoro <unifwizidoro@P3LB0804.FEINET.FEI.EDU.BR>
2024-10-09 15:03:58 -0300

    testando na main

--- exchenge-cripto/src/exchenge/cripto/ExchengeCripto.java
+++ exchenge-cripto/src/exchenge/cripto/ExchengeCripto.java
@@ -14,7 +14,7 @@ public class ExchengeCripto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            System.out.println("Testando o github...");
     }
     
 }
