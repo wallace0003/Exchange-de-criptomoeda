6d455128b1c70daf11ff69e52a3c90b051d67b7f
wallace0003 <wallaceizidoro.1@gmail.com>
2024-10-30 18:17:34 -0300

    Mostrando nome do usuário no menu.

--- exchenge-cripto/src/controller/ControllerLogin.java
+++ exchenge-cripto/src/controller/ControllerLogin.java
@@ -15,38 +15,44 @@ public class ControllerLogin {
     private MenuFrame menuFrame;
 
     public ControllerLogin(LoginFrame loginFrame,
-            MenuFrame menuFrame) {
+                           MenuFrame menuFrame){
             this.loginFrame = loginFrame;
             this.menuFrame = menuFrame;
     }
     
     public void realizarLogin(){
-        Investidor investidor = new 
-            Investidor(loginFrame.getTextSenha().getText(),
-            null, null, loginFrame.getTextCpf().getText());
-        Conexao conexao = new Conexao();
-        try{
-            Connection conn = conexao.getConnection();
-            InvestidorDAO dao = new InvestidorDAO(conn);
-            ResultSet res = dao.consultar(investidor);
-            if(res.next()){
-                JOptionPane.showMessageDialog(loginFrame, "Login efetuado",
-                            "aviso", JOptionPane.INFORMATION_MESSAGE);
-                loginFrame.setVisible(false);
-                menuFrame.setVisible(true);
-                
-            } else{
-                JOptionPane.showMessageDialog(loginFrame, 
-                                             "Senha ou CPF incorreto!",
-                                            "Erro" , JOptionPane.ERROR_MESSAGE);
-            }
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(loginFrame, "Erro de conexão: " 
-                                          + e.getMessage(),
-                                          "Erro", JOptionPane.ERROR_MESSAGE);
+    Investidor investidor = new 
+        Investidor(loginFrame.getTextSenha().getText(), null, null,
+                   loginFrame.getTextCpf().getText());
+    Conexao conexao = new Conexao();
+    try {
+        Connection conn = conexao.getConnection();
+        InvestidorDAO dao = new InvestidorDAO(conn);
+        ResultSet res = dao.consultar(investidor);
+        
+        if (res.next()) {
+            // Captura o nome e o CPF do usuário logado
+            String nome = res.getString("nome");
+            String cpf = res.getString("cpf");
+            
+            JOptionPane.showMessageDialog(loginFrame, "Login efetuado", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+            
+            // Fechar a janela de login e abrir a janela do menu
+            loginFrame.setVisible(false);
+            
+            // Passa os dados para a próxima janela
+            menuFrame.setjLNome(nome);
+            menuFrame.setjLCpf(cpf);
+            menuFrame.setVisible(true);
+            
+        } else {
+            JOptionPane.showMessageDialog(loginFrame, "Senha ou CPF incorreto!", "Erro", JOptionPane.ERROR_MESSAGE);
         }
+    } 
+    catch (SQLException e) {
+        JOptionPane.showMessageDialog(loginFrame, "Erro de conexão: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
     }
-    
+    }
     
     
     
